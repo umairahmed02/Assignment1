9a61ae448a86d04bf2a1f6b8e51773ef1c7c70c5
umairahmed02 <umi1045@gmail.com>
2022-05-17 10:58:53 +1200

    Fixed it slightly

--- app/src/main/java/com/example/assignment1/Subject1.java
+++ app/src/main/java/com/example/assignment1/Subject1.java
@@ -14,8 +14,13 @@ import android.widget.Button;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -25,12 +30,26 @@ public class Subject1 extends AppCompatActivity {
     Button btnAdd;
     ArrayList <Note> noteArrayList = new ArrayList<>();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    Query query = db.collection("Note").whereEqualTo("subject", "Subject1");
+
+    /*ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+            for(DataSnapshot ds : snapshot.getChildren()) {
+
+            }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject1);
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
 
         btnAdd = findViewById(R.id.btn_add);
         db.collection("Note")
@@ -46,13 +65,14 @@ public class Subject1 extends AppCompatActivity {
                             noteArrayList.add(note);
                             Log.v("Arraysnote", note.getBody());
                         }
+                        RecyclerView recyclerView = findViewById(R.id.recycler_view);
                         Log.v("Arraysnotesize", noteArrayList.toString());
+                        Log.v("Arraysfinal", noteArrayList.toString());
+                        RecyclerViewAdapter adapter = new RecyclerViewAdapter(Subject1.this, noteArrayList);
+                        recyclerView.setAdapter(adapter);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(Subject1.this));
                     }
                 });
-        Log.v("Arraysfinal", noteArrayList.toString());
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, noteArrayList);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         getSupportActionBar().setTitle("Subject1");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
