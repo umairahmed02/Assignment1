2cd102ec48d9c1815a0ea43c544c6190b422f5cf
umairahmed02 <umi1045@gmail.com>
2022-05-20 22:46:33 +1200

    Could not fix only 1 entry showing but transferred the code to all other subjects.

---
+++ app/src/main/java/com/example/assignment1/ChangePassword.java
@@ -0,0 +1,67 @@
+package com.example.assignment1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class ChangePassword extends AppCompatActivity {
+
+    EditText oldPassword, password, confirmPassword;
+    Button btnChangePassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+
+        password = findViewById(R.id.txt_password);
+        oldPassword = findViewById(R.id.txt_oldpass);
+        confirmPassword = findViewById(R.id.txt_password_confirm);
+        btnChangePassword = findViewById(R.id.btn_changepassword);
+
+        btnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String pass = password.getText().toString();
+                String oldPass = oldPassword.getText().toString();
+                String confirmPass = confirmPassword.getText().toString();
+
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                if(pass.length() < 8) {
+                    Toast.makeText(ChangePassword.this, "Password is not of sufficient length", Toast.LENGTH_SHORT).show();
+                }
+                else if(confirmPass.length() < 8) {
+                    Toast.makeText(ChangePassword.this, "Confirm password is not of sufficient length", Toast.LENGTH_SHORT).show();
+                }
+                else if(!(pass.equals(confirmPass))) {
+                    Toast.makeText(ChangePassword.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    db.collection("User")
+                            .whereEqualTo("username", User.getInstance("u", "p", "e").getUsername())
+                            .get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        DocumentSnapshot document = task.getResult().getDocuments().get(0);
+                                        //document.get("password") = pass;
+                                    }
+                                }
+        });
+    }
+
+
+}
